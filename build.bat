ea9db754aba2ebe60a74d5018c7665a96c052908
Timothy Ghanem <timothy.ghanim@gmail.com>
2016-11-21 14:36:46 +0200

    Restoring continuous integration to Travis CI.

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set configuration=Release
-
-set version=1.0.0
-if not "%PackageVersion%" == "" (set version=%PackageVersion%)
-
-"%PROGRAMFILES(X86)%\MSBuild\14.0\bin\msbuild.exe" pst\pst.sln /p:Configuration="%configuration%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
-
-nuget pack pst.nuspec -NoPackageAnalysis -verbosity detailed -Version %version% -p configuration="%configuration%"
