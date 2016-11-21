29391d73eab4aa2e20e81617ebd90ccb3fd683a9
Timothy Ghanem <timothy.ghanim@gmail.com>
2016-11-21 11:03:52 +0200

    Fixing the name for the config replacement token.

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @echo off
-set config=Release
+set configuration=Release
 
 set version=1.0.0
 if not "%PackageVersion%" == "" (set version=%PackageVersion%)
 
-%PROGRAMFILES(X86)%\MSBuild\14.0\bin\msbuild.exe pst\pst.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%PROGRAMFILES(X86)%\MSBuild\14.0\bin\msbuild.exe pst\pst.sln /p:Configuration="%configuration%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 nuget pack pst.nuspec -NoPackageAnalysis -verbosity detailed -Version %version%
