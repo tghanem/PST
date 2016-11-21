46d9bec5b7eb1e4985ba7b4b1d1fcc4ee0ba834d
Timothy Ghanem <timothy.ghanim@gmail.com>
2016-11-21 10:59:47 +0200

    Removing unnecessary steps from the build script

--- build.bat
+++ build.bat
@@ -1,16 +1,7 @@
 @echo off
-set config=%1
-if "%config%" == "" (set config=Release)
-
 set version=1.0.0
 if not "%PackageVersion%" == "" (set version=%PackageVersion%)
 
-set nuget= if "%nuget%" == "" (set nuget=nuget)
+%PROGRAMFILES(X86)%\MSBuild\14.0\bin\msbuild.exe pst\pst.sln /p:Configuration="Release" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
-%PROGRAMFILES(X86)%\MSBuild\14.0\bin\msbuild.exe pst\pst.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
-
-mkdir Build
-mkdir Build\lib
-mkdir Build\lib\net452
-
-%nuget% pack "pst.nuspec" -NoPackageAnalysis -verbosity details -o Build -Version %version% -p Configuration="%config%"
+nuget pack pst.nuspec -NoPackageAnalysis -verbosity detailed -Version %version%
