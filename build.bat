375c35d19d6daa94b1074cd04bfb28f1a74bc651
Timothy Ghanem <timothy.ghanim@gmail.com>
2016-11-21 10:13:58 +0200

    Creating automated CI for NuGet

---
+++ build.bat
@@ -0,0 +1,16 @@
+@echo off
+set config=%1
+if "%config%" == "" (set config=Release)
+
+set version=1.0.0
+if not "%PackageVersion%" == "" (set version=%PackageVersion%)
+
+set nuget= if "%nuget%" == "" (set nuget=nuget)
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild pst\pst.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net452
+
+%nuget% pack "pst.nuspec" -NoPackageAnalysis -verbosity details -o Build -Version %version% -p Configuration="%config%"
