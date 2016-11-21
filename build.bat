1da60e822647f92679af68e11da07dc17f1399b2
Timothy Ghanem <timothy.ghanim@gmail.com>
2016-11-21 12:48:15 +0200

    Setting the configuration replacement correctly in the build.bat

--- build.bat
+++ build.bat
@@ -6,4 +6,4 @@ if not "%PackageVersion%" == "" (set version=%PackageVersion%)
 
 "%PROGRAMFILES(X86)%\MSBuild\14.0\bin\msbuild.exe" pst\pst.sln /p:Configuration="%configuration%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
-nuget pack pst.nuspec -NoPackageAnalysis -verbosity detailed -Version %version%
+nuget pack pst.nuspec -NoPackageAnalysis -verbosity detailed -Version %version% -p configuration="%configuration%"
